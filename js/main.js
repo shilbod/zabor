23d954ed82daba22c3f8c61822d5158eeb4b4a50
shilbod <38855687+shilbod@users.noreply.github.com>
2021-03-04 05:37:38 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,113 @@
 $(document).ready(function() {
     $("#phone").mask("+375 (99) 999-99-99");
 });
+
+
+
+//Ymap start
+var spinner = $('.ymap-container').children('.loader');
+var check_if_load = 0;
+var myMapTemp, myPlacemarkTemp;
+
+
+function init () {
+  var myMapTemp = new ymaps.Map("map-yandex", {
+    center: [53.902284, 27.561831],
+    zoom: 11,
+    controls: ['zoomControl', 'fullscreenControl']
+  });
+
+
+  var myPlacemarkTemp = new ymaps.Placemark([53.931260, 27.758778], {
+      balloonContent: "Здесь может быть ваш адрес",
+  }, {
+  });
+  
+  myMapTemp.geoObjects.add(myPlacemarkTemp);
+
+  //Получаем первый экземпляр коллекции слоев, потом первый слой коллекции
+  var layer = myMapTemp.layers.get(0).get(0);
+
+  //Решение по callback-у для определния полной загрузки карты: http://ru.stackoverflow.com/questions/463638/callback-загрузки-карты-yandex-map
+  waitForTilesLoad(layer).then(function() {
+    //Скрываем
+    spinner.removeClass('is-active');
+  });
+}
+
+function waitForTilesLoad(layer) {
+  return new ymaps.vow.Promise(function (resolve, reject) {
+    var tc = getTileContainer(layer), readyAll = true;
+    tc.tiles.each(function (tile, number) {
+      if (!tile.isReady()) {
+        readyAll = false;
+      }
+    });
+    if (readyAll) {
+      resolve();
+    } else {
+      tc.events.once("ready", function() {
+        resolve();
+      });
+    }
+  });
+}
+
+function getTileContainer(layer) {
+  for (var k in layer) {
+    if (layer.hasOwnProperty(k)) {
+      if (
+        layer[k] instanceof ymaps.layer.tileContainer.CanvasContainer
+        || layer[k] instanceof ymaps.layer.tileContainer.DomContainer
+      ) {
+        return layer[k];
+      }
+    }
+  }
+  return null;
+}
+
+function loadScript(url, callback){
+
+  var script = document.createElement("script");
+
+  if (script.readyState){  //IE
+    script.onreadystatechange = function(){
+      if (script.readyState == "loaded" ||
+              script.readyState == "complete"){
+        script.onreadystatechange = null;
+        callback();
+      }
+    };
+  } else {  //Другие браузеры
+    script.onload = function(){
+      callback();
+    };
+  }
+
+  script.src = url;
+  document.getElementsByTagName("head")[0].appendChild(script);
+}
+
+var ymap = function() {
+  $('.ymap-container').mouseenter(function(){
+      if (check_if_load == 0) {
+        check_if_load = 1;
+
+        spinner.addClass('is-active');
+
+        loadScript("https://api-maps.yandex.ru/2.1/?lang=ru_RU&amp;loadByRequire=1", function(){
+           ymaps.load(init);
+        });         
+       
+      }
+    }
+  );  
+}
+
+$(function() {
+
+  //Map Yandex
+  ymap();
+
+});
